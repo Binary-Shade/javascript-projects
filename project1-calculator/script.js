c82bdf9d395b7ab9fd7cf81520f348616b2dc7d5
suresh k <115919438+Binary-Shade@users.noreply.github.com>
2023-10-07 22:00:21 +0530

    html & css files added

---
+++ project1-calculator/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+const specialChars = ["/", "%", "*", "+", "-", "="];
+let output = "";
+
+const calculate = () =>{
+    
+}

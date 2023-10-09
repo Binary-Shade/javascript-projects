135d3bf1d30bcfd2e373626f7f3a4bb1f0cad355
suresh k <115919438+Binary-Shade@users.noreply.github.com>
2023-10-09 20:56:16 +0530

    project completed

--- project1-calculator/script.js
+++ project1-calculator/script.js
@@ -1,8 +1,26 @@
-const button = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
-const specialChars = ["/", "%", "*", "+", "-", "="];
+const specialChars = ["%", "/", "*", "+", "-"];
 let output = "";
 
-const calculate = () =>{
-    
+const calculate = (btnValue) => {
+    // console.log(btnValue);
+    display.focus();
+    if(btnValue === "=" && btnValue != ""){
+        output = eval(output.replace("%", "/100"));
+    }else if(btnValue === "AC"){
+        output = "";
+    }else if(btnValue === "DEL"){
+        output = output.toString().slice(0, -1);
+    }else{
+        if(output === "" && specialChars.includes(btnValue)){
+            return;
+        }
+    output += btnValue;
+    }
+    display.value = output;
 }
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value)) 
+});
